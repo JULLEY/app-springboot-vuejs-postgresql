6725d1b1142f860955b2c8d374b94d6b1fed386b
WOONG <ung91@yahoo.com>
2022-01-11 00:39:13 +0900

    add mapList method

--- src/main/java/com/programming/app/controller/api/MainApiController.java
+++ src/main/java/com/programming/app/controller/api/MainApiController.java
@@ -4,15 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class MainApiController {
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "hello!!!!!";
-    }
-    @GetMapping("/map")
-    public String mapList() {
-        return "mapList";
+
+    @GetMapping("/map/list")
+    public List<Map<String, Object>> mapList() {
+        ArrayList<Map<String, Object>> resList = new ArrayList<>();
+        return resList;
     }
 }
