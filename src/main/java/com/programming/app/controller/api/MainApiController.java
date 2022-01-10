d1531ce0134b803ed59805caeb91b00119125a4f
WOONG <ung91@yahoo.com>
2022-01-11 00:32:26 +0900

    add mapList method

--- src/main/java/com/programming/app/controller/api/MainApiController.java
+++ src/main/java/com/programming/app/controller/api/MainApiController.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class MainApiController {
-
     @GetMapping("/hello")
     public String helloWorld() {
         return "hello!!!!!";
     }
-
     @GetMapping("/map")
     public String mapList() {
         return "mapList";
     }
-
 }
